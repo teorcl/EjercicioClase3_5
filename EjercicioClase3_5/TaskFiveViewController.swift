8f4a1e233dc48424a522c03df3bcaaf280d5f84b
Teodoro <teo.calle.lara@gmail.com>
2022-11-08 14:06:48 -0500

    Se  crean los métodos para mostrar y cambiar el nombre con cada click

---
+++ EjercicioClase3_5/TaskFiveViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TaskFiveViewController.swift
+//  EjercicioClase3_5
+//
+//  Created by TEO on 8/11/22.
+//
+
+import UIKit
+
+class TaskFiveViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var counter = 0
+    let names = ["Luis","Gloria","David","Natalia"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showNames()
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        play()
+    }
+    
+    func showNames(){
+        nameLabel.text = "\(names[counter])"
+    }
+    
+    func play(){
+        showNames()
+        changeCounter()
+    }
+    
+    func changeCounter(){
+        if(counter>=3){
+            counter = 0
+        }else{
+            counter += 1
+        }
+    }
+}
+
